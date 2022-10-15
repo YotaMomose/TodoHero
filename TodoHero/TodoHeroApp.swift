e7a670abcb438a1b68f9c9103346e8e137c5859a
Yota Momose <tonkatsu440@gmail.com>
2022-10-15 18:30:04 +0900

    Initial Commit

---
+++ TodoHero/TodoHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoHeroApp.swift
+//  TodoHero
+//
+//  Created by Apple on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct TodoHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
