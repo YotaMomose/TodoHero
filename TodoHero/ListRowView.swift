02a54f526bddb003f30a1972b1b2397f11637402
Yota Momose <tonkatsu440@gmail.com>
2022-10-15 19:52:17 +0900

    ListRowを作成

--- TodoHero/ListRowView.swift
+++ TodoHero/ListRowView.swift
@@ -9,9 +9,87 @@ import SwiftUI
 
 struct ListRowView: View {
     let task: String
-    var checked: Bool
+    var IsCheck: Bool
+    let monstar: Int
     var body: some View {
         HStack {
+            
+            Text(IsCheck ? "☑︎" : "□")
+            if IsCheck {
+                Text(task)
+                    .strikethrough()
+                    .fontWeight(.ultraLight)
+                switch monstar {
+                case 1 :
+                    Image("slime")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.gray)
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                        
+                case 2 :
+                    Image("orc")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.gray)
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                case 3 :
+                    Image("golem")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.gray)
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                case 4 :
+                    Image("dragon")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.gray)
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                default:
+                    Image("maou")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.gray)
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                }
+                // defaultをなんとかしたい
+            } else {
+                Text(task)
+                switch monstar {
+                case 1 :
+                    Image("slime")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                        
+                case 2 :
+                    Image("orc")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                case 3 :
+                    Image("golem")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                case 4 :
+                    Image("dragon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                default:
+                    Image("maou")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40,height: 40)
+                }
+                // defaultをなんとかしたい
+            }
             
         }
     }
@@ -19,6 +97,6 @@ struct ListRowView: View {
 
 struct ListRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ListRowView()
+        ListRowView(task: "掃除", IsCheck: true, monstar: 4)
     }
 }
---
+++ TodoHero/TodoManager.swift
@@ -0,0 +1,20 @@
+//
+//  TodoManager.swift
+//  TodoHero
+//
+//  Created by Apple on 2022/10/15.
+//
+
+import SwiftUI
+
+class TodoManager: ObservableObject {
+    
+}
+
+enum monster {
+    case slime
+    case orc
+    case golem
+    case dragon
+    case maou
+}
