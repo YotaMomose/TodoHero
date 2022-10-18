6705f6e68d860cdaa2074f5dfb1682a97ff22d74
Yota Momose <tonkatsu440@gmail.com>
2022-10-18 17:37:03 +0900

    紫注意修正
    チェックボタンの挙動に問題あり

--- TodoHero/ListView.swift
+++ TodoHero/ListView.swift
@@ -12,30 +12,44 @@ struct ListView: View {
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Task.timestamp, ascending: true)],
                   animation: .default) var items: FetchedResults<Task>
     @EnvironmentObject var todoManager : TodoManager
+    @State var isPresented = false
+    @State var isShowAction = false
     
     var body: some View {
         NavigationStack {
             List {
-                if self.todoManager.isEditing {
+                if isPresented {
                     AddTaskView()
                 } else {
                     Button(action:{
-                        self.todoManager.isEditing = true
+                        isPresented = true
                     }) {
                         Text("＋")
                             .font(.title)
                     }
-                    
+                    .onReceive(todoManager.$isEditing) { isPresented in
+                        self.isPresented = isPresented
+                    }
                 }
                 
                 ForEach(items) { item in
                     Button(action:{
-                        item.checked = true
+                        isShowAction = true
                         //確認画面が出るように修正
                     }) {
                         if item.task?.isEmpty == false {
                             ListRowView(task:item.task! , IsCheck: item.checked, monstar: Int(item.monster))
                         }
+                    }
+                    .confirmationDialog("確認", isPresented: $isShowAction, titleVisibility: .hidden) {
+                        Button("倒した！") {
+                            item.checked = true
+                        }
+                        Button("まだ倒してない") {
+                            isShowAction = false
+                        }
+                    } message: {
+                        Text("タスクを倒しましたか？").bold()
                     }
                     
                 }
